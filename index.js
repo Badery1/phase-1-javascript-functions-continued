0714fdd52841c06a1abb65dce4758decb45dde97
barit <mmyers.com@Gmail.com>
2023-08-21 15:39:04 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun(something="roller-skate") {
+    return `This Saturday, I want to ${something}!`;
+}
+function mondayWork(somethingElse="go to the office") {
+    return `This Monday, I will ${somethingElse}.`;
+}
+const wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+        return `You are ${style}${adjective}${style}!`;
+    }
+}
